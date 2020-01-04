f856c0ffa4dc56bd6dd3e98e65c4d774477e2936
dhankar_infobot <infobot2016@gmail.com>
2020-01-04 13:10:30 +0530

    init_1

---
+++ Assets/Scripts/SimpleRotate.js
@@ -0,0 +1,6 @@
+function Update()
+{
+// page 96
+    transform.Rotate(0,5,0);
+
+}
---
+++ Assets/Scripts/simpleRotate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class simpleRotate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.Rotate(0,5,0);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
